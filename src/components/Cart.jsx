1d5fe6a4bfba72a4909455d2ecd2a6a9408c3596
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-09-01 22:22:55 +0500

    responsive : true

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,17 +19,17 @@ const Cart = () => {
         <div className='w-[95%] mx-auto  '>
             <h1 className='text-4xl my-5 font-semibold text-gray-700'>Welcome to Cart</h1>
             <hr className=" border-black" />
-            <div className='my-7 mx-2'>
+            <div className='my-7 sm:mx-2'>
                 <ul className='list-none'>
                     {products && products.map((product) =>
-                        <li className='py-2 bg-teal-200 rounded-md px-2 my-2' key={product.id}>
-                            <div className='flex justify-between'>
-                                <div className='flex justify-center  space-x-8 w-max align-middle '>
+                        <li className='py-2 bg-teal-200 rounded-md px-2 my-2 ' key={product.id}>
+                            <div className='sm:flex sm:justify-between  '>
+                                <div className='flex justify-center  space-x-8  align-middle '>
                                     <img src={product.images[0]} alt="" className='rounded-md w-20 h-24' />
-                                    <p className='font-semibold text-gray-700 text-xl my-auto'>{product.title}</p>
+                                    <p className='font-semibold text-gray-700 sm:text-xl text-base my-auto'>{product.title}</p>
                                 </div>
-                                <div className='flex my-auto align-middleborderw-80 justify-end '>
-                                    <div className='my-auto mr-5  text-gray-700 text-xl'>
+                                <div className='flex my-auto align-middleborderw-80 sm:justify-end justify-center sm:w-auto px-3 items-center sm:px-0'>
+                                    <div className='my-auto mr-5  text-gray-700 sm:text-xl text-sm'>
                                         Price : {product.price}/Rs
                                     </div>
                                     <div>
@@ -41,15 +41,18 @@ const Cart = () => {
                     )}
 
                 </ul>
-                <div className=' text-gray-700 text-xl border-b border-gray-700 my-4 flex justify-between'>
+                <div className=' text-gray-700 sm:text-xl text-base border-b border-gray-700 my-4 flex justify-between'>
                     <div>Total Price</div>
                     <div>{ totalPrice }/Rs.___</div>
                 </div>
               <div className='flex justify-between my-auto items-center'>
                 <p className='font-bold text-gray-600'>Just One Step back of placing order</p>
-            <div className='space-x-3 outline-none' >
-              <button className='py-1 px-4 bg-cyan-800 hover:bg-cyan-950  text-lg   rounded-md text-white' onClick={() => handleClearCart()}>Clear Cart</button>
-              <button className='py-1 px-4 bg-green-600 hover:bg-green-700  text-lg   rounded-md text-white' >Checkout</button>
+            <div className='sm:space-x-3 sm:space-y-0 sy space-y-2 outline-none flex flex-col sm:flex-row  justify-center items-center' >
+
+              <button className='py-1 px-4 bg-cyan-800 hover:bg-cyan-950 sm:text-lg text-base   rounded-md text-white' onClick={() => handleClearCart()}>Clear Cart</button>
+
+              <button className='py-1 px-4 bg-green-600 hover:bg-green-700   sm:text-lg text-base   rounded-md text-white' >Checkout</button>
+
               </div>
               </div>
             </div>
--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../store/slices/cartSlice'
 import { Link } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../assets/App.css'
 
@@ -28,24 +28,24 @@ const ProductView = () => {
     
   }
   return (
-    <div className=' mx-auto flex flex-col bg-slate-300 pt-5 justify-center align-middle px-5 space-y-5 pb-16'>
+    <div className=' mx-auto flex flex-col bg-slate-300 pt-5  justify-center align-middle px-5 space-y-5 pb-3 sm:pb-8'>
       <h1 className='text-slate-700 text-4xl'>Product Overview</h1>
 
-      <div className='flex justify-center space-x-10 mt-5'>
-        <img src={product.images[0]} alt="" className='w-[26%] h-70 rounded-md' />
-        <img src={product.images[1]} alt="" className='w-[26%] h-70 rounded-md' />
-        <img src={product.images[2]} alt="" className='w-[26%] h-70 rounded-md' />
+      <div className='flex sm:flex-row flex-col items-center space-x-0 space-y-3  justify-center sm:space-x-10 sm:space-y-0 mt-5'>
+        <img src={product.images[0]} alt="" className='sm:w-[26%] w-full  sm:h-70 h-52 rounded-md' />
+        <img src={product.images[1]} alt="" className='sm:w-[26%] w-full  sm:h-70 h-52 rounded-md' />
+        <img src={product.images[2]} alt="" className='sm:w-[26%] w-full  sm:h-70 h-52 rounded-md' />
       </div>
 
-      <div className='flex justify-between '>
-        <div className='flex flex-col space-y-3 mt-10'>
-          <h1 className='text-3xl ml-5 font-bold text-gray-700 '>{product.title}</h1>
-          <p className='ml-5 w-[60%] text-gray-700'>{product.description}</p>
+      <div className='flex justify-between flex-col sm:flex-row '>
+        <div className='flex flex-col space-y-3 mt-10 sm:items-start  items-center '>
+          <h1 className='text-3xl sm:ml-5 ml-0 font-bold text-gray-700 '>{product.title}</h1>
+          <p className='sm:ml-5 sm:w-[60%] text-gray-700 sm:text-left text-center'>{product.description}</p>
         </div>
-        <div className='flex flex-col justify-center align-middle text-center'>
+        <div className='flex flex-col justify-center sm:align-middle items-center text-center'>
           <h1 className='text-3xl font-bold  w-64 mt-10' >Price: {product.price} Rs</h1>
-          <div className='flex flex-col space-y-3 my-3'>
-            <button className='bg-green-600 px-10 py-3  rounded-md hover:cursor-pointer hover:bg-green-700' onClick={() => {
+          <div className='flex flex-col space-y-3 my-3 w-52'>
+            <button className='bg-green-600 px-10 py-3  rounded-md  hover:cursor-pointer hover:bg-green-700' onClick={() => {
               addToCartFunc();
               showNotification()
             }}>Add to Cart</button>
