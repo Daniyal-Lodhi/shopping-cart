398f7aa04f1e68aa899f64a5bc2bddec96af22c6
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-09-18 17:58:18 +0500

    .

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
          <span className='font-bold text-gray-800 sm:text-3xl  ' >Welcome to Shopping Cart</span> <br/> 
         </h1>
         <p className='text-center text-sm sm:text-base mx-10 mt-3'>
-        Shopping Cart is a mini react js project by <a href="https://www.linkedin.com/in/daniyal-lodhi-77b995242/"> <p className='inline underline '>Daniyal lodhi</p></a>. It is a simple cart made for practice purpose, The states of products gallery, Overview and checkout are fully managed by <b>redux toolkit</b> library. Further more it also supports the checkout feature by <b>Stripe </b>where you can enter a test credit card number, any future date and any 3 digit CVC code and the checkout will proceed. It is in test mode because real world checkout will also need a real world business. 
+        Shopping Cart is a mini react js project by <a href="https://www.linkedin.com/in/daniyal-lodhi-77b995242/"> <p className='inline underline '>Daniyal lodhi</p></a>. It is a simple shopping cart made for practice purpose, The states of products gallery, Overview and checkout are fully managed by <b>redux toolkit</b> library. Further more it also supports the checkout feature by <b>Stripe </b>where you can enter a test credit card number, any future date and any 3 digit CVC code and the checkout will proceed. It is in test mode because real world checkout will also need a real world business. 
         </p>
         <Link className='rounded-md text-sm  px-3 py-2 hover:bg-green-700 flex items-center bg-green-600 my-7 ' to ='/productgallery' >
           Click to Browse items 
